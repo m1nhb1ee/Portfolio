83a2162899ebe8e083cb146dc3160195166dd6b7
m1nhb1ee <minh.nt235976@sis.hust.edu.vn>
2026-03-05 02:27:04 +0700

    fix pic

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { Download, ChevronDown } from 'lucide-react';
-import profileImg from 'C:/Job/Personal Project/Portfolio/src/assets/23cd5281e8ec5a5a9a556ff998d569db29b99635.jpg';
+import profileImg from '../../assets/23cd5281e8ec5a5a9a556ff998d569db29b99635.jpg';
 
 export function Hero() {
   const scrollToSection = (href: string) => {
